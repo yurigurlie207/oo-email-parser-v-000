84e3f3160e1da59b1a2dc3718fd8b3b3edbd16eb
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-12 20:35:39 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,27 +8,27 @@ class EmailParser
 
  attr_accessor :emails, :email_list
 
-def initialize(emails)
-  @emails = emails
-end
-
-
-def parse
-
-  @email_list = @emails.split(', ')
-
-  if @email_list.size < 2
-    @email_list = @emails.split(' ')
-  else
-    @email_list.each do |item|
-      item.split(' ').each do |item2|
-        @email_list.pop(item)
-        @email_list << item2
-      end
-    end
+  def initialize(emails)
+    @emails = emails
   end
 
-  @email_list.uniq
-end
+
+  def parse
+
+    @email_list = @emails.split(', ')
+
+    if @email_list.size < 2
+      @email_list = @emails.split(' ')
+    else
+      @email_list.each do |item|
+        item.split(' ').each do |item2|
+          @email_list.pop(item)
+          @email_list << item2
+        end
+      end
+    end
+
+    @email_list.uniq
+  end
 
 end
